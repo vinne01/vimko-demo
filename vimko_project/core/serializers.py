533bbd5fb451f20b818d4f2f490ae71aa2312334
vinay kumar maurya <113638115+vinne01@users.noreply.github.com>
2026-03-26 19:11:57 +0530

    Update serializers.py

--- vimko_project/core/serializers.py
+++ vimko_project/core/serializers.py
@@ -17,10 +17,7 @@ class DealerSerializer(serializers.ModelSerializer):
         model = Dealer
         fields = '__all__'
 
-# class OrderItemSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = OrderItem
-#         fields = ['id', 'product', 'quantity', 'unit_price', 'line_total']
+
 class OrderItemSerializer(serializers.ModelSerializer):
     unit_price = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
     line_total = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
@@ -34,38 +31,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Quantity must be greater than zero.")
         return value     
 
-# class OrderSerializer(serializers.ModelSerializer):
-#     items = OrderItemSerializer(many=True)
-#     class Meta:
-#         model = Order
-#         fields = ['id', 'dealer', 'order_number', 'status', 'total_amount', 'items']
 
-#     def create(self, validated_data):
-#         items_data = validated_data.pop('items')
-#         order = Order.objects.create(**validated_data)
-#         total_amount = 0
-#         for item_data in items_data:
-#             item = OrderItem.objects.create(order=order, **item_data)
-#             total_amount += item.line_total
-#         order.total_amount = total_amount
-#         order.save()
-#         return order
-
-#     def update(self, instance, validated_data):
-#         if instance.status != 'Draft':
-#             raise serializers.ValidationError("Cannot edit non-draft order.")
-#         items_data = validated_data.pop('items', [])
-#         instance.dealer = validated_data.get('dealer', instance.dealer)
-#         instance.save()
-#         # Update order items
-#         instance.items.all().delete()
-#         total_amount = 0
-#         for item_data in items_data:
-#             item = OrderItem.objects.create(order=instance, **item_data)
-#             total_amount += item.line_total
-#         instance.total_amount = total_amount
-#         instance.save()
-#         return instance
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
